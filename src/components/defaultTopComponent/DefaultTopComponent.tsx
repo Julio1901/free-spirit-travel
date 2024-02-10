a41d7084fa4a28e11608f58c0b7ee2a590e47bcb
Julio Cesar <js.cesar42@gmail.com>
2024-02-10 02:23:28 -0300

    feat: top component implemented without shopping button finished

--- src/components/defaultTopComponent/DefaultTopComponent.tsx
+++ src/components/defaultTopComponent/DefaultTopComponent.tsx
@@ -4,12 +4,25 @@ export const DefaultTopComponent = () => {
     return(
         <div className={styles.MainContainer}>
             <div className={styles.ContentContainer}>
-                <div className={styles.ImageContainer}>
+                <div className={styles.LogoContainer}>
                     <img src="/assets/images/logo.png"/>
                 </div>
                 <div className={styles.RightContentContainer}>
-                      {/* TODO: Remove this harded code and implement external API to get real value */}
-                        <p className={styles.Text}>Cotação dólar hoje: R$5,53</p>
+                    <p className={styles.DollarDescriptionText}>Cotação dólar hoje: R$5,53</p>
+                    <div className={styles.FlagContainer}>
+                        <img src="/assets/images/brazil-flag.png"/>
+                    </div>
+                    <div className={styles.QuestionBallonContainer}>
+                        <img src="/assets/images/question-ballon.png"/>
+                    </div>
+                    <div className={styles.Divider}></div>
+                    <div className={styles.PersonIconContainer}>
+                        <img src="/assets/images/person-icon.png"/>
+                    </div>
+                    <p className={styles.EnterText}>Entrar</p>
+                    <div className={styles.ShoppingButtonContainer}>
+
+                    </div>
                 </div>
             </div>
         </div>
