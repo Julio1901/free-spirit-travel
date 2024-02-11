1bcf2f629c16840a5feba18a9d455cfcb26c7cd0
Julio Cesar <js.cesar42@gmail.com>
2024-02-11 02:04:20 -0300

    tech: first simple test created

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,9 +1,9 @@
 import {render, screen } from "@testing-library/react";
 import Home from "@/app/page";
-
+import '@testing-library/jest-dom';
 
 it("Should render correctly", () => {
     render(<Home/>)
 
-
+    expect(screen.getByText("Entrar")).toBeInTheDocument()
 })
