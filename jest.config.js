e7912aa9f983131c8e66623452bcb9963f407bd7
Julio Cesar <js.cesar42@gmail.com>
2024-02-11 02:00:42 -0300

    tech: initial test configuration created

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require("next/jest.js");
+ 
+const createJestConfig = nextJest({
+  // Forneça o caminho para o seu aplicativo Next.js para carregar next.config.js e arquivos .env no ambiente de teste
+  dir: './',
+});
+
+/**
+ * @type {import('jest').Config}
+ */
+const config = {
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+    "testEnvironment": "jsdom",
+    preset: "ts-jest",
+};
+ 
+// createJestConfig é exportado desta forma para garantir que next/jest possa carregar o configuração Next.js que é assíncrona
+module.exports = createJestConfig(config);
