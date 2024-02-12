d15262def801230ba671897f3dd6e994c4e53998
Julio Cesar <js.cesar42@gmail.com>
2024-02-12 12:55:47 -0300

    feat: search click action implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+'use client'
+
 import { DefaultTopComponent } from "@/components/defaultTopComponent/DefaultTopComponent";
 import "./styles/home.css"
 import { DefaultSearchComponent } from "@/components/defaultSearchComponent/DefaultSearchComponent";
 
 export default function Home() {
+
+  const handleSearchClicked = (value : string) => {
+    console.log(value)
+  }
+
   return (
    <div className="MainContainer">
       <DefaultTopComponent/>
-      <DefaultSearchComponent/>
+      <DefaultSearchComponent onSearchClicked={ e => handleSearchClicked(e)}/>
    </div>
   );
 }
--- src/components/defaultSearchComponent/DefaultSearchComponent.tsx
+++ src/components/defaultSearchComponent/DefaultSearchComponent.tsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import styles from "./styles.module.css"
 
-export const DefaultSearchComponent = () => {
+interface IDefaultSearchComponentProps {
+    onSearchClicked : (value: string) => void
+}
+
+export const DefaultSearchComponent: React.FC<IDefaultSearchComponentProps> = ({onSearchClicked}) => {
+
+    const [searchText, setSearchText] = useState('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchText(event.target.value);
+    };
+
+
+    const handleSearchClick = () => {
+        onSearchClicked(searchText);
+    };
+
+
     return(
         <div className={styles.MainContainer}>
             <div className={styles.InputContainer}>
                 <div className={styles.MapIconContainer}>
                     <img src="/assets/images/map-icon.png"/>
                 </div>
-                <input type="text" placeholder="Busque por atração" />
-                <div className={styles.SearchIconContainer} data-testid="id-search-icon-container">
+                <input type="text" placeholder="Busque por atração" onChange={handleInputChange}/>
+                <div className={styles.SearchIconContainer} data-testid="id-search-icon-container" onClick={handleSearchClick}>
                     <img src="/assets/images/search-icon.png" alt="Search Icon"/>
                 </div>
             </div>
