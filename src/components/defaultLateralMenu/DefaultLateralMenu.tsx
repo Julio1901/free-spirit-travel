ec5841eab573ea395eb7bb670ee832aa0be6c6be
Julio Cesar <js.cesar42@gmail.com>
2024-02-13 13:54:41 -0300

    feat: price button group created

--- src/components/defaultLateralMenu/DefaultLateralMenu.tsx
+++ src/components/defaultLateralMenu/DefaultLateralMenu.tsx
@@ -7,8 +7,18 @@ export const DefaultLateralMenu : React.FC = () => {
                 <p className={styles.Title}>Filtro</p>
                 <p className={styles.ButtonText}>Limpar todos os filtros</p>
             </div>
-
             <div className={styles.Divider} style={{marginTop: 12}}></div>
+            <div className={styles.PriceFilterContainer}>
+               <p className={styles.PriceTitle}>Preço</p>
+               <div className={styles.PriceButtonGroupContainer}>
+                    <div className={styles.PriceButtonFilled}>R$ 10,00 - R$ 390,00</div>
+                    <div className={styles.PriceButtonNotFilled}>R$ 10,00 - R$ 390,00</div>
+                    <div className={styles.PriceButtonNotFilled}>R$ 10,00 - R$ 390,00</div>
+                    <div className={styles.PriceButtonNotFilled}>R$ 10,00 - R$ 390,00</div>
+               </div>
+            </div>
+            <div className={styles.Divider} style={{marginTop: 24}}></div>
+        
            
         </div>
     )
