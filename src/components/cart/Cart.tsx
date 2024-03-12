a92509d24fd430365dd63a7792c43b2ba03c9eb3
Julio Cesar <js.cesar42@gmail.com>
2024-03-12 12:26:36 -0300

    feat: cart finished before removed background color

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -31,6 +31,36 @@ export default function Cart(){
                 </div>
             </div>
            <div className={styles.Divider} style={{marginInline: 24}}/>
+           <div className={styles.BodyContainer}>
+            <div className={styles.BodyTextsContainer}>
+                <div className={styles.BodyContentContainer}>
+                    <p className={styles.BodyContainerTextNormal}>Ingressos</p>
+                    <p className={styles.BodyContainerTextNormal}>R$1.468,60</p>
+                </div>
+                <div className={styles.BodyContentContainer}>
+                    <p className={styles.BodyContainerTextBold}>Subtotal</p>
+                    <p className={styles.BodyContainerTextBold}>R$ 1.468,60</p>
+                </div>
+                <div className={styles.BodyContentContainer}>
+                    <p className={styles.BodyContainerTextNormal}>1X de R$1.365,79,00 com desconto de  <span style={{ color: '#00919E' }}>(7%)</span></p>
+                    <p className={styles.BodyContainerDiscountText}>-R$102,80</p>
+                </div>
+                <div className={styles.BodyContentContainer}>
+                    <p className={styles.BodyContainerTextNormal}>10X Sem juros de R$1.468,60</p>
+                    <p className={styles.BodyContainerTextNormal}>R$1.468,60</p>
+                </div>
+
+            </div>
+
+            <div className={styles.Divider} style={{marginTop: 24}}/>
+            <div className={styles.TotalValueContainer}>
+                <p className={styles.TotalValueContainerText}>Valor total</p>
+                <p className={styles.TotalValueContainerPriceText}>1.468,60</p>
+            </div>
+           </div>
+           <div className={styles.GoToCheckOutButtonContainer}>
+                <p className={styles.GoToCheckOutButtonText}>Ir para o check out</p>
+           </div>
         </div>
     )
 }
