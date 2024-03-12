bbeb327511f36f1c9dbda0be10dae0f45a453580
Julio Cesar <js.cesar42@gmail.com>
2024-03-12 13:25:09 -0300

    feat: card changed to display all items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,12 +82,52 @@ export default function Home() {
     );
   }
 
+
+
+const mock = [
+
+    {
+      "id": "1",
+      "name": "Museu de Arte Moderna",
+      "location": "Rio de Janeiro, RJ",
+      "image": "https://i.postimg.cc/zD6RYYc7/museu-de-arte.jpg",
+      "description": "O Museu de Arte Moderna é um espaço cultural dedicado à exibição de obras de arte moderna e contemporânea, destacando-se pela diversidade de artistas e estilos.",
+      "price": {
+        "full": 2351.28,
+        "discount": 1391.28
+      },
+      "rating": {
+        "reviewsCount": 345,
+        "value": 4.3
+      },
+      "createdAt": "2022-08-31T12:32:09.794Z",
+      "updatedAt": "2022-08-31T12:32:09.794Z"
+    },
+    {
+      "id": "2",
+      "name": "Torre Eiffel",
+      "location": "Paris, França",
+      "image": "https://i.postimg.cc/JhFkqsSN/torre-eiffel.jpg",
+      "description": "A Torre Eiffel é um ícone da arquitetura mundial, situada em Paris, oferecendo uma vista deslumbrante da cidade luz a partir de seus diferentes níveis.",
+      "price": {
+        "full": 2351.28,
+        "discount": 1391.28
+      },
+      "rating": {
+        "reviewsCount": 345,
+        "value": 4.3
+      },
+      "createdAt": "2022-08-31T12:32:09.794Z",
+      "updatedAt": "2022-08-31T12:32:09.794Z"
+    }
+]
+
   return (
     <Provider store={store}>
          <PersistGate loading={null} persistor={persistor}>
          <div className="MainContainer">
       <DefaultTopComponent/>
-      <Cart/>
+      <Cart items={mock}/>
       <DefaultSearchComponent hint="Busque por atração" onSearchClicked={ e => handleSearchClicked(e)}/>
       <div className="BodyContainer">
         <div className="LateralMenuContainer">
--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,69 +1,85 @@
 import styles from "./styles.module.css"
 
-export default function Cart(){
-    return(
+interface ICartProps{
+    items: ITicket[]
+}
+
+const Cart: React.FC<ICartProps> = ({items}) => {
+    return (
         <div className={styles.MainContainer}>
-            <div className={styles.TopContentContainer}>
-                <p className={styles.Title}>Ingressos</p>
-                <div className={styles.TopContentContainerBody}>
-                    <img src="/assets/images/profile-image.png" />
-                    <div className={styles.TopContentContainerBodyTextsContainer}>
-                        <div className={styles.TopContentContainerBodyTextsContainerHeader}>
-                            <p className={styles.TicketTitle}>Ingresso Universal 2 park to 2 dias flexivel - 15/09/2022</p>
-                            <img src="/assets/icons/icon-garbage.png"/>
-                        </div>  
-                        <div className={styles.TopContentContainerBodyPricesTextContainer}>
-                            <p className={styles.TopContentContainerBodyPricesText}>1 Adulto: R$500,00</p>
-                            <p className={styles.TopContentContainerBodyPricesText}>2 Crianças: R$234,330</p>
+            {items.length !== 0 ? 
+                (<>
+                {items.map((item) => (
+                <><div className={styles.TopContentContainer}>
+                        <p className={styles.Title}>Ingressos</p>
+                        <div className={styles.TopContentContainerBody}>
+                            <img src="/assets/images/profile-image.png" />
+                            <div className={styles.TopContentContainerBodyTextsContainer}>
+                                <div className={styles.TopContentContainerBodyTextsContainerHeader}>
+                                    <p className={styles.TicketTitle}>Ingresso Universal 2 park to 2 dias flexivel - 15/09/2022</p>
+                                    <img src="/assets/icons/icon-garbage.png" />
+                                </div>
+                                <div className={styles.TopContentContainerBodyPricesTextContainer}>
+                                    <p className={styles.TopContentContainerBodyPricesText}>1 Adulto: R$500,00</p>
+                                    <p className={styles.TopContentContainerBodyPricesText}>2 Crianças: R$234,330</p>
+                                </div>
+                                <div className={styles.Divider} style={{ marginTop: 8 }} />
+                                <div className={styles.QuantityPurchasedContainer}>
+                                    <div className={styles.QuantityPurchasedContentContainer}>
+                                        <p className={styles.QuantityPurchasedContainerTotalText}>Qtd 02</p>
+                                        <p className={styles.QuantityPurchasedContainerTotalText}>R$ 3.999,00</p>
+                                    </div>
+                                    <div className={styles.QuantityPurchasedContentContainer}>
+                                        <p className={styles.QuantityPurchasedContainerSubtotalText}>Subtotal</p>
+                                        <p className={styles.QuantityPurchasedContainerSubtotalTextValue}>R$ 1.468,60</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                       <div className={styles.Divider} style={{marginTop: 8}}/>
-                        <div className={styles.QuantityPurchasedContainer}>
-                        <div className={styles.QuantityPurchasedContentContainer}>
-                            <p className={styles.QuantityPurchasedContainerTotalText}>Qtd 02</p>
-                            <p className={styles.QuantityPurchasedContainerTotalText}>R$ 3.999,00</p>
+                    </div><div className={styles.Divider} style={{ marginInline: 24 }} /><div className={styles.BodyContainer}>
+                            <div className={styles.BodyTextsContainer}>
+                                <div className={styles.BodyContentContainer}>
+                                    <p className={styles.BodyContainerTextNormal}>Ingressos</p>
+                                    <p className={styles.BodyContainerTextNormal}>R$1.468,60</p>
+                                </div>
+                                <div className={styles.BodyContentContainer}>
+                                    <p className={styles.BodyContainerTextBold}>Subtotal</p>
+                                    <p className={styles.BodyContainerTextBold}>R$ 1.468,60</p>
+                                </div>
+                                <div className={styles.BodyContentContainer}>
+                                    <p className={styles.BodyContainerTextNormal}>1X de R$1.365,79,00 com desconto de  <span style={{ color: '#00919E' }}>(7%)</span></p>
+                                    <p className={styles.BodyContainerDiscountText}>-R$102,80</p>
+                                </div>
+                                <div className={styles.BodyContentContainer}>
+                                    <p className={styles.BodyContainerTextNormal}>10X Sem juros de R$1.468,60</p>
+                                    <p className={styles.BodyContainerTextNormal}>R$1.468,60</p>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.Divider} style={{ marginTop: 24 }} />
+                            <div className={styles.TotalValueContainer}>
+                                <p className={styles.TotalValueContainerText}>Valor total</p>
+                                <p className={styles.TotalValueContainerPriceText}>1.468,60</p>
+                            </div>
                         </div>
-                        <div className={styles.QuantityPurchasedContentContainer}>
-                            <p className={styles.QuantityPurchasedContainerSubtotalText}>Subtotal</p>
-                            <p className={styles.QuantityPurchasedContainerSubtotalTextValue}>R$ 1.468,60</p>
+                        </>
+                ))}
+                         <div className={styles.GoToCheckOutButtonContainer}>
+                            <p className={styles.GoToCheckOutButtonText}>Ir para o check out</p>
                         </div>
-                        </div>
-                     
+
+                        </>
+            )
+            : null }
 
 
-                    </div>
-                </div>
-            </div>
-           <div className={styles.Divider} style={{marginInline: 24}}/>
-           <div className={styles.BodyContainer}>
-            <div className={styles.BodyTextsContainer}>
-                <div className={styles.BodyContentContainer}>
-                    <p className={styles.BodyContainerTextNormal}>Ingressos</p>
-                    <p className={styles.BodyContainerTextNormal}>R$1.468,60</p>
-                </div>
-                <div className={styles.BodyContentContainer}>
-                    <p className={styles.BodyContainerTextBold}>Subtotal</p>
-                    <p className={styles.BodyContainerTextBold}>R$ 1.468,60</p>
-                </div>
-                <div className={styles.BodyContentContainer}>
-                    <p className={styles.BodyContainerTextNormal}>1X de R$1.365,79,00 com desconto de  <span style={{ color: '#00919E' }}>(7%)</span></p>
-                    <p className={styles.BodyContainerDiscountText}>-R$102,80</p>
-                </div>
-                <div className={styles.BodyContentContainer}>
-                    <p className={styles.BodyContainerTextNormal}>10X Sem juros de R$1.468,60</p>
-                    <p className={styles.BodyContainerTextNormal}>R$1.468,60</p>
-                </div>
 
-            </div>
 
-            <div className={styles.Divider} style={{marginTop: 24}}/>
-            <div className={styles.TotalValueContainer}>
-                <p className={styles.TotalValueContainerText}>Valor total</p>
-                <p className={styles.TotalValueContainerPriceText}>1.468,60</p>
-            </div>
-           </div>
-           <div className={styles.GoToCheckOutButtonContainer}>
-                <p className={styles.GoToCheckOutButtonText}>Ir para o check out</p>
-           </div>
+
+           
         </div>
     )
 }
+
+export default Cart;    
